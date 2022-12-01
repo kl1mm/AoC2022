8d6af0ddfc18aa3293434b47ae981e7ccb3a6e7d
Tobi Kli <tobias.klimm@txs.de>
2022-12-01 09:20:38 +0100

    Day1

---
+++ kli.AoC2022/Day_1/Part1.cs
@@ -0,0 +1,17 @@
+﻿namespace kli.AoC2022.Day_1;
+
+[TestFixture]
+public class Part1
+{
+    [Test]
+    public async Task Part_1()
+    {
+        var input = await File.ReadAllTextAsync(Path.Combine("Day_1", "input.txt"));
+        var result = input.Split("\r\n\r\n")
+            .Select(g => g.Split("\r\n")
+                .Sum(int.Parse))
+            .Max();
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
---
+++ kli.AoC2022/Day_1/Part2.cs
@@ -0,0 +1,19 @@
+﻿namespace kli.AoC2022.Day_1;
+
+[TestFixture]
+public class Part2
+{
+    [Test]
+    public async Task Part_1()
+    {
+        var input = await File.ReadAllTextAsync(Path.Combine("Day_1", "input.txt"));
+        var result = input.Split("\r\n\r\n")
+            .Select(g => g.Split("\r\n")
+                .Sum(int.Parse))
+            .OrderDescending()
+            .Take(3)
+            .Sum();
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
