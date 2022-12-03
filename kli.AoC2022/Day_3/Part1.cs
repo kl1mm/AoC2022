5ff62f95f409f08de1133480764333f8233651d9
Tobi Kli <tobias.klimm@txs.de>
2022-12-03 19:06:58 +0100

    Day3

---
+++ kli.AoC2022/Day_3/Part1.cs
@@ -0,0 +1,32 @@
+﻿namespace kli.AoC2022.Day_3;
+
+[TestFixture]
+public class Part1
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_3", "input.txt"));
+
+        int FindItem(string s)
+        {
+            var head = s[..(s.Length / 2)];
+            var tail = s[(s.Length / 2)..];
+            foreach (var charFront in head.ToArray())
+            {
+                foreach (var charEnd in tail.Reverse().ToArray())
+                {
+                    if (charFront.Equals(charEnd))
+                        return charFront - (char.IsLower(charFront) ? 97 : 65-26) + 1;
+                }
+            }
+
+            throw new NotSupportedException();
+        }
+
+        var result = inputLines.Sum(FindItem);
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
---
+++ kli.AoC2022/Day_3/Part2.cs
@@ -0,0 +1,34 @@
+﻿namespace kli.AoC2022.Day_3;
+
+[TestFixture]
+public class Part2
+{
+    [Test]
+    public async Task Test()
+    {
+        int FindItem(string one, string two, string three)
+        {
+            foreach (var item in one)
+            {
+                if (two.Contains(item, StringComparison.Ordinal) && three.Contains(item, StringComparison.Ordinal))
+                    return item - (char.IsLower(item) ? 97 : 65 - 26) + 1;
+            }
+
+            throw new Exception();
+        }
+
+        var result = 0;
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_3", "input.txt"));
+        for (int i = 0; i < inputLines.Length; i += 3)
+        {
+            var one = inputLines[i];
+            var two = inputLines[i+1];
+            var three = inputLines[i+2];
+
+            result += FindItem(one, two, three);
+        }
+
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
