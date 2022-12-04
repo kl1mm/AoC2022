a75a9bcc9dc287425e3dc26f12da26032f50a6a2
Tobi Kli <tobias.klimm@txs.de>
2022-12-04 16:43:48 +0100

    Day4

---
+++ kli.AoC2022/Day_4/Part1.cs
@@ -0,0 +1,29 @@
+﻿namespace kli.AoC2022.Day_4;
+
+[TestFixture]
+public class Part1
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_4", "input.txt"));
+
+        HashSet<int> ToSet(int start, int end) => new(Enumerable.Range(start, end - start +1));
+
+        bool IsSubset(string line)
+        {
+            var left = line.Split(',').First();
+            var right = line.Split(',').Last();
+
+            var leftSet = ToSet(int.Parse(left.Split('-').First()), int.Parse(left.Split('-').Last()));
+            var rightSet = ToSet(int.Parse(right.Split('-').First()), int.Parse(right.Split('-').Last()));
+
+            return leftSet.IsSubsetOf(rightSet) || rightSet.IsSubsetOf(leftSet);
+        }
+        
+        var result = inputLines.Where(IsSubset).Count();
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
---
+++ kli.AoC2022/Day_4/Part2.cs
@@ -0,0 +1,29 @@
+﻿namespace kli.AoC2022.Day_4;
+
+[TestFixture]
+public class Part2
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_4", "input.txt"));
+
+        HashSet<int> ToSet(int start, int end) => new(Enumerable.Range(start, end - start +1));
+
+        bool Overlaps(string line)
+        {
+            var left = line.Split(',').First();
+            var right = line.Split(',').Last();
+
+            var leftSet = ToSet(int.Parse(left.Split('-').First()), int.Parse(left.Split('-').Last()));
+            var rightSet = ToSet(int.Parse(right.Split('-').First()), int.Parse(right.Split('-').Last()));
+
+            return leftSet.Overlaps(rightSet);
+        }
+        
+        var result = inputLines.Where(Overlaps).Count();
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
