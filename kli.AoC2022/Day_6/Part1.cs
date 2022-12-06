bf69dbc7be6adf836b504f0e48e2504bc6579f57
Tobi Kli <tobias.klimm@txs.de>
2022-12-06 09:07:10 +0100

    Day6

---
+++ kli.AoC2022/Day_6/Part1.cs
@@ -0,0 +1,22 @@
+﻿namespace kli.AoC2022.Day_6;
+
+[TestFixture]
+public class Part1
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLine = await File.ReadAllTextAsync(Path.Combine("Day_6", "input.txt"));
+        var result = 0;
+        for (int i = 0; i < inputLine.Length; i++)
+        {
+            if (inputLine[i..].Take(4).Distinct().Count() == 4)
+            {
+                result = i + 4;
+                break;
+            }
+        } 
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
---
+++ kli.AoC2022/Day_6/Part2.cs
@@ -0,0 +1,24 @@
+﻿namespace kli.AoC2022.Day_6;
+
+[TestFixture]
+public class Part2
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLine = await File.ReadAllTextAsync(Path.Combine("Day_6", "input.txt"));
+        
+        var result = 0;
+        for (int i = 0; i < inputLine.Length; i++)
+        {
+            if (inputLine[i..].Take(14).Distinct().Count() == 14)
+            {
+                result = i + 14;
+                break;
+            }
+        }        
+
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
+
