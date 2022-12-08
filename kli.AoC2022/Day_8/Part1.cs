6135ebf109d9aa526653250db7b7c5f87ba46e1a
Tobi Kli <tobias.klimm@txs.de>
2022-12-08 16:10:51 +0100

    Day8

---
+++ kli.AoC2022/Day_8/Part1.cs
@@ -0,0 +1,27 @@
+﻿namespace kli.AoC2022.Day_8;
+
+[TestFixture]
+public class Part1
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_8", "input.txt"));
+
+        List<List<int>> trees = inputLines.Select(line => line.Select(chr => chr - '0').ToList()).ToList();
+
+        int result = 0;
+        for (int i = 0; i < trees.Count; i++)
+        {
+            for (int j = 0; j < trees.Count; j++)
+            {
+                if (trees.Select(t => t[j]).Take(i).All(t => t < trees[i][j])) { result++; continue; }
+                if (trees.Select(t => t[j]).Skip(i + 1).All(t => t < trees[i][j])) { result++; continue; }
+                if (trees[i].Take(j).All(t => t < trees[i][j])) { result++; continue; } 
+                if (trees[i].Skip(j + 1).All(t => t < trees[i][j])) { result++; }
+            }
+        }
+        
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
---
+++ kli.AoC2022/Day_8/Part2.cs
@@ -0,0 +1,50 @@
+﻿namespace kli.AoC2022.Day_8;
+
+[TestFixture]
+public class Part2
+{
+    [Test]
+    public async Task Test()
+    {
+        var inputLines = await File.ReadAllLinesAsync(Path.Combine("Day_8", "input.txt"));
+        
+        var trees = new List<List<int>>();
+        foreach (string line in inputLines)
+            trees.Add(line.Select(x => x - '0').ToList());
+
+        var result = -1;
+        for (int i = 0; i < trees.Count; i++)
+        {
+            for (int j = 0; j < trees[i].Count; j++)
+            {
+                var upCount = 0;
+                for (int u = i - 1; u >= 0; u--)
+                {
+                    upCount++;
+                    if (trees[i][j] <= trees[u][j]) break;
+                }
+                var downCount = 0;
+                for (int d = i + 1; d < trees.Count; d++)
+                {
+                    downCount++;
+                    if (trees[i][j] <= trees[d][j]) break;
+                }
+                var leftCount = 0;
+                for (int l = j - 1; l >= 0; l--)
+                {
+                    leftCount++;
+                    if (trees[i][j] <= trees[i][l]) break;
+                }
+                var rightCount = 0;
+                for (int r = j + 1; r < trees[i].Count; r++)
+                {
+                    rightCount++;
+                    if (trees[i][j] <= trees[i][r]) break;
+                }
+
+                result = Math.Max(result, upCount * downCount * leftCount * rightCount);
+            }
+        }
+        await TestContext.Out.WriteLineAsync($"The result is: {result}");
+    }
+}
